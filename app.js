51eceafa7072c4018dc61b1b31455479205acc31
sebastien grobon <sebastien.grobon@lapiscine.pro>
2023-04-03 14:29:04 +0200

    second

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-
-
 const express = require('express')
 const app = express()
 const port = 3080
 
 app.get('/monChemin', (req, res) => {
-    console.log("j'ai reçu une requête")
-  res.send('Hello World!')
+    console.log("j'ai reçu une requête appuie moi sur la tronche!!!!!")
+    res.send('Réponse pour /monChemin')
+})
+
+app.get('/monCheminPerdu', (req, res) => {
+    console.log("j'ai reçu une requête et je te rentre dedans!!!!!")
+    res.send('Hello World!,  I am the king of the World!!!!!')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
 
 
 
